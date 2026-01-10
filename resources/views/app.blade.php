ef19acf510b7ab74f9194d5a690fa8cdeb0c3406
Gladys Hersaputri <gladys@MacBook-Air-Gladys.local>
2026-01-10 23:17:04 +0700

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Memo Mandiri</title>
+        @viteReactRefresh
+        @vite('resources/js/app.jsx', 'resources/css/app.css')
+        @inertiaHead
+    </head>
+    <body>
+        @inertia
+    </body>
+</html>
